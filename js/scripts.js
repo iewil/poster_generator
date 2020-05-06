4735bb12150739023621d06ece82f62ce77aaedf
iewil <liwei@data.gov.sg>
2020-05-06 23:13:18 +0800

    Remove business name

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ var qr_code = new QRCode(document.getElementById("qrcode"))
 
 function updatePage() {
   qr_code.clear()
-  var company_name = document.getElementById("company_name").value;
+  // var company_name = document.getElementById("company_name").value;
   var safe_entry_url = document.getElementById("safe_entry_url").value;
   qr_code.makeCode(safe_entry_url)
 }
