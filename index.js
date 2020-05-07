aa2b5a45ae9182adf474fa3dfa513d1ee371b3b2
iewil <liwei@data.gov.sg>
2020-05-07 20:22:19 +0800

    remove handler code

--- index.js
+++ index.js
@@ -4,25 +4,26 @@ const PDFDocument = require('pdfkit');
 const QRCode = require('qrcode')
 const slugify = require('slugify')
 
-exports.handler =  async function(event, context) {
+async function generatePDF(params) {
+  console.log("starting script")
   try {
-    console.log("start")
     // Create a document
+    console.log("creating pdf doc")
     let doc = new PDFDocument({
       size: "A3",
       margin: 0,
     });
-    let qr_description = "NTUC Northpoint SE"
-    let company_logo_url = "https://s3-ap-southeast-1.amazonaws.com/www.fairprice.com.sg/fpol/media/images/wcm/corporate/Corporate_logo.png"
-    let safe_entry_url = "https://temperaturepass.ndi-api.gov.sg/login/PROD-S83CS0191L-NTUC-NORTHPOINT-SE"
+    let qr_description = params.qr_description;
+    let company_logo_url = params.company_logo_url;
+    let safe_entry_url = params.safe_entry_url;
     let qr_code_image = await QRCode.toDataURL(safe_entry_url, { 
         errorCorrectionLevel: 'H',
         quality: 1,
         margin: 1,
       }
-    )
-    let poster_template_path = "assets/poster_template.jpg"
-    let safe_entry_logo_path = "assets/SafeEntry_logo_inline.png"
+    );
+    let poster_template_path = "assets/poster_template.jpg";
+    let safe_entry_logo_path = "assets/SafeEntry_logo_inline.png";
 
     // Add poster template
     doc.image(poster_template_path, {
@@ -67,31 +68,21 @@ exports.handler =  async function(event, context) {
       });
       
     doc.end();
+    
+    console.log("completed pdf doc")
 
     // Pipe its output somewhere, like to a file or HTTP response
-    console.log('/tmp/' + slugify(qr_description) + '.pdf')
     doc.pipe(fs.createWriteStream('/tmp/' + slugify(qr_description) + '.pdf'));
+    console.log("saved pdf doc to: " + '/tmp/' + slugify(qr_description) + '.pdf')
 
-    let buffer = fs.readFileSync('/tmp/' + slugify(qr_description) + '.pdf').toString('BASE64');
-    let file_data = fs.existsSync('/tmp/NTUC-Northpoint-SE.pdf')
-    console.log(file_data)
-
-    console.log(fs.readFileSync('/tmp/' + slugify(qr_description) + '.pdf'))
-
-    let response = {
-      statusCode: 200,
-      headers: {'Content-type' : 'application/pdf'},
-      body: buffer,
-      isBase64Encoded : true,
-    };
-
-    // return response
-    console.log(response)
-    return response;
   } catch (error) {
     console.log(error)
     throw Error(error)
   }
 }
 
-exports.handler("event", "context")
+generatePDF({
+  'qr_description': "NTUC Northpoint SE",
+  'company_logo_url': "https://s3-ap-southeast-1.amazonaws.com/www.fairprice.com.sg/fpol/media/images/wcm/corporate/Corporate_logo.png",
+  'safe_entry_url': "https://temperaturepass.ndi-api.gov.sg/login/PROD-S83CS0191L-NTUC-NORTHPOINT-SE"
+})
